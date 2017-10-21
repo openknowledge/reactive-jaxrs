7db70ca268cd82bdda41f8ed2248214673f782ef
Arne Limburg <arne.limburg@openknowledge.de>
2017-10-21 13:37:02 +0200

    Add JAX-B

--- reactive-jaxrs/src/test/java/de/openknowledge/jaxrs/reactive/test/Customer.java
+++ reactive-jaxrs/src/test/java/de/openknowledge/jaxrs/reactive/test/Customer.java
@@ -12,6 +12,12 @@
  */
 package de.openknowledge.jaxrs.reactive.test;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Customer {
   private String firstName;
   private String lastName;
