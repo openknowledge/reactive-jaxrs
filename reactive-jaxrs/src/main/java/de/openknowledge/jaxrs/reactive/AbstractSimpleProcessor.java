606d46d72fc191994e421d1db95ac43c4bc8b611
CSchulz <christian.schulz@openknowledge.de>
2017-10-21 20:51:41 +0200

    add license and cleanup method

--- reactive-jaxrs/src/main/java/de/openknowledge/jaxrs/reactive/AbstractSimpleProcessor.java
+++ reactive-jaxrs/src/main/java/de/openknowledge/jaxrs/reactive/AbstractSimpleProcessor.java
@@ -1,13 +1,24 @@
+/*
+ * Copyright (C) open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package de.openknowledge.jaxrs.reactive;
 
 import java.util.concurrent.Flow;
 
-/**
- * @author Christian Schulz - open knowledge GmbH
- */
 public abstract class AbstractSimpleProcessor<T, R> implements Flow.Processor<T, R> {
 
   private boolean error = false;
+  private boolean canceled = false;
+
   private Flow.Subscription parentSubscription;
   private Flow.Subscription childSubscription;
   private Flow.Subscriber<? super R> childSubscriber;
@@ -20,7 +31,8 @@ public abstract class AbstractSimpleProcessor<T, R> implements Flow.Processor<T,
       public void cancel() {
         super.cancel();
 
-        childSubscriber = null;
+        canceled = true;
+        cleanup();
       }
     };
     subscriber.onSubscribe(childSubscription);
@@ -42,8 +54,9 @@ public abstract class AbstractSimpleProcessor<T, R> implements Flow.Processor<T,
   public void onError(Throwable throwable) {
     if (childSubscriber != null) {
       childSubscriber.onError(throwable);
-      childSubscriber = null;
+
       error = true;
+      cleanup();
     }
   }
 
@@ -54,9 +67,19 @@ public abstract class AbstractSimpleProcessor<T, R> implements Flow.Processor<T,
     }
   }
 
+  private void cleanup() {
+    childSubscriber = null;
+    childSubscription = null;
+
+  }
+
   protected abstract R process(T item);
 
-  public boolean hasError() {
+  public boolean isErred() {
     return error;
+  }
+
+  public boolean isCanceled() {
+    return canceled;
   }
 }
